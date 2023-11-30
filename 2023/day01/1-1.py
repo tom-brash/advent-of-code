daf1639449ff37b015f905c4e13e71d6f2f2218f
Tom Brash <brash.tom@bcg.com>
2023-12-01 10:28:41 +1100

    Adding 2023 folder

---
+++ 2023/day01/1-1.py
@@ -0,0 +1,9 @@
+def main():
+    print('==== Day 1 ====')
+    with open('input', 'r') as open_file:
+	    elves = open_file.read().strip().split('\n\n')
+
+    print(f'\n(1-1) The elf with the most calories has: {max} calories')
+
+if __name__ == "__main__":
+	main()
